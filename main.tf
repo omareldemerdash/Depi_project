6b887df02eb73ea4458cb0048d5a6d42d8655a8c
omareldemerdash <omareldemerdash28.gmail.com>
2025-04-11 19:24:17 +0200

    add terraform

---
+++ main.tf
@@ -0,0 +1,191 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+# VPC and networking setup
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "route_assoc" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "swarm_sg" {
+  name   = "swarm-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2377
+    to_port     = 2377
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "tcp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    self        = true
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Key Pair
+resource "aws_key_pair" "deployer" {
+  key_name   = "my_key"
+  public_key = file("my_key.pub")
+}
+
+# IAM Role for EC2
+resource "aws_iam_role" "ssm_role" {
+  name = "swarm_ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "swarm_profile"
+  role = aws_iam_role.ssm_role.name
+}
+
+# S3 Bucket for token
+resource "aws_s3_bucket" "bucket" {
+  bucket = "swarm-bucket-${random_id.bucket_id.hex}"
+  acl    = "private"
+}
+
+# Manager EC2
+resource "aws_instance" "manager" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.swarm_sg.id]
+  key_name               = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              systemctl enable docker
+              usermod -a -G docker ec2-user
+              docker swarm init --advertise-addr $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+              docker swarm join-token worker -q > /tmp/swarm_token.txt
+              aws s3 cp /tmp/swarm_token.txt s3://${aws_s3_bucket.bucket.bucket}/swarm_token.txt
+              EOF
+
+  tags = {
+    Name = "Swarm-Manager"
+  }
+}
+
+# Worker EC2
+resource "aws_instance" "worker" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.swarm_sg.id]
+  key_name               = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+
+  depends_on = [aws_instance.manager]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              service docker start
+              systemctl enable docker
+              usermod -a -G docker ec2-user
+              sleep 60
+              TOKEN=$(aws s3 cp s3://${aws_s3_bucket.bucket.bucket}/swarm_token.txt -)
+              docker swarm join --token $TOKEN ${aws_instance.manager.private_ip}:2377
+              EOF
+
+  tags = {
+    Name = "Swarm-Worker"
+  }
+}
